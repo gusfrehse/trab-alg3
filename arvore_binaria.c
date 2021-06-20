93b4c979982fd583aea506356ed400a79f5042e4
vinimribeiro <vinicius.mribeiro13@gmail.com>
2021-06-19 21:51:46 -0300

    Added support functions

--- arvore_binaria.c
+++ arvore_binaria.c
@@ -1,6 +1,24 @@
+#include <stdlib.h>
 #include "arvore_binaria.h"
 
 tNo *criar_arvore(const char *expressao)
+{
+    int indice_inicial = 0;
+    return criar_arvore_aux(expressao, &indice_inicial);
+}
+
+tNo *criar_no(int chave)
+{
+    tNo *no = malloc(sizeof(tNo));
+
+    no->chave = chave;
+    no->dir = NULL;
+    no->esq = NULL;
+
+    return no;
+}
+
+tNo *criar_arvore_aux(const char *expressao)
 {
     const char *chave = expressao + 1;
 
