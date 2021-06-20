8665dda16f81de8ba5b649343fe94fe2c72cf915
Gustavo S Frehse <gusfrehse@gmail.com>
2021-06-19 21:45:07 -0300

    Finished implementation of cria_arvore_aux().

--- arvore_binaria.c
+++ arvore_binaria.c
@@ -1,16 +1,27 @@
+#include <ctype.h>
 #include "arvore_binaria.h"
 
-tNo *criar_arvore(const char *expressao)
+tNo *criar_arvore_aux(const char *expressao, int *indice)
 {
-    const char *chave = expressao + 1;
+    *indice++;
+    const char *chave = expressao + indice;
 
     tNo *no = criar_no(chave);
 
-    if (!isdigit(chave))
+    if (isdigit(chave))
     {
-        no->esq = criar_arvore(primeiro_argumento(expressao));
-        no->dir = criar_arvore(segundo_argumento(expressao));
+	while (isdigit(expressao[*indice]))
+	{
+	    *indice++;
+	}
     }
+    else
+    {
+        no->esq = criar_arvore_aux(expressao, indice);
+        no->dir = criar_arvore_aux(expressao, indice);
+    }
+
+    *indice++;
 
     return no;
 }
