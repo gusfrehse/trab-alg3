e994a164cc62da740dac5187cc62ca98154e2af0
vinimribeiro <vinicius.mribeiro13@gmail.com>
2021-06-20 20:52:25 -0300

    User input can now be read. Yay!

--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include "arvore_binaria.h"
 
+#define MAX_INPUT_SIZE 256
+
 int main()
 {
-    char* str = "(*(5)(+(4)(3))";
+    //char* str = "(*(5)(+(4)(3))";
+    char input[MAX_INPUT_SIZE];
 
-    tNo* arvore = criar_arvore(str);
+    fgets(input, MAX_INPUT_SIZE - 1, stdin);
+
+    tNo *arvore = criar_arvore(input);
     float resultado = resolver_avore(arvore);
     printf("%.2f\n", resultado);
     liberar_arvore(arvore);
